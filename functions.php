637d3dd1187511e78de4c293f3460c5bd4a9bb2f
Максим Процюк <protsuk26@gmail.com>
2025-04-24 23:25:29 +0300

    створення кастомного типу постів а також заміна виклику в page-portfolio

--- functions.php
+++ functions.php
@@ -211,235 +211,238 @@ function theme_customize_register($wp_customize)
     ));
   }
 
+}
+ // Реєстрація типу запису "Послуги"
+ function register_services_post_type() {
+  $labels = array(
+      'name'               => 'Послуги',
+      'singular_name'      => 'Послуга',
+      'menu_name'          => 'Послуги',
+      'add_new'            => 'Додати нову',
+      'add_new_item'       => 'Додати нову послугу',
+      'edit_item'          => 'Редагувати послугу',
+      'new_item'           => 'Нова послуга',
+      'view_item'          => 'Переглянути послугу',
+      'search_items'       => 'Шукати послуги',
+      'not_found'          => 'Послуги не знайдено',
+      'not_found_in_trash' => 'В кошику послуг не знайдено',
+  );
 
-  $wp_customize->add_section('work_section', array(
-    'title' => 'Секція "Чим ми займаємося"',
-    'priority' => 30,
-  ));
+  $args = array(
+      'labels'             => $labels,
+      'public'             => true,
+      'publicly_queryable' => true,
+      'show_ui'            => true,
+      'show_in_menu'       => true,
+      'query_var'          => true,
+      'rewrite'            => array('slug' => 'services'),
+      'capability_type'    => 'post',
+      'has_archive'        => true,
+      'hierarchical'       => false,
+      'menu_position'      => 5,
+      'menu_icon'          => 'dashicons-hammer',
+      'supports'           => array('title', 'thumbnail'),
+  );
 
-  $wp_customize->add_setting('work_section_title', array(
-    'default' => '',
-    'sanitize_callback' => 'sanitize_text_field',
-  ));
+  register_post_type('services', $args);
+}
+add_action('init', 'register_services_post_type');
 
-  $wp_customize->add_control('work_section_title', array(
-    'label' => 'Заголовок секції',
-    'section' => 'work_section',
-    'type' => 'text',
-  ));
+// Додаємо метабокс для тексту картки
+function services_meta_boxes() {
+  add_meta_box(
+      'service_card_text',
+      'Текст картки',
+      'service_card_text_callback',
+      'services',
+      'normal',
+      'high'
+  );
+}
+add_action('add_meta_boxes', 'services_meta_boxes');
 
-  $wp_customize->add_setting('work_card1_text', array(
-    'default' => '',
-    'sanitize_callback' => 'sanitize_text_field',
-  ));
-
-  $wp_customize->add_control('work_card1_text', array(
-    'label' => 'Текст першої картки',
-    'section' => 'work_section',
-    'type' => 'text',
-  ));
-
-  $wp_customize->add_setting('work_card1_image', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'work_card1_image', array(
-    'label' => 'Зображення першої картки',
-    'section' => 'work_section',
-  )));
-
-  $wp_customize->add_setting('work_card1_image_2x', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'work_card1_image_2x', array(
-    'label' => 'Зображення першої картки (2x)',
-    'section' => 'work_section',
-  )));
-
-  $wp_customize->add_setting('work_card2_text', array(
-    'default' => '',
-    'sanitize_callback' => 'sanitize_text_field',
-  ));
-
-  $wp_customize->add_control('work_card2_text', array(
-    'label' => 'Текст другої картки',
-    'section' => 'work_section',
-    'type' => 'text',
-  ));
-
-  $wp_customize->add_setting('work_card2_image', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'work_card2_image', array(
-    'label' => 'Зображення другої картки',
-    'section' => 'work_section',
-  )));
-
-  $wp_customize->add_setting('work_card2_image_2x', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'work_card2_image_2x', array(
-    'label' => 'Зображення другої картки (2x)',
-    'section' => 'work_section',
-  )));
-
-  $wp_customize->add_setting('work_card3_text', array(
-    'default' => '',
-    'sanitize_callback' => 'sanitize_text_field',
-  ));
-
-  $wp_customize->add_control('work_card3_text', array(
-    'label' => 'Текст третьої картки',
-    'section' => 'work_section',
-    'type' => 'text',
-  ));
-
-  $wp_customize->add_setting('work_card3_image', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'work_card3_image', array(
-    'label' => 'Зображення третьої картки',
-    'section' => 'work_section',
-  )));
-
-  $wp_customize->add_setting('work_card3_image_2x', array(
-    'default' => '',
-    'sanitize_callback' => 'esc_url_raw',
-  ));
-
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'work_card3_image_2x', array(
-    'label' => 'Зображення третьої картки (2x)',
-    'section' => 'work_section',
-  )));
-
-  $wp_customize->add_section('team_section', array(
-    'title' => 'Секція "Наша команда"',
-    'priority' => 40,
-  ));
-
-
-  $wp_customize->add_setting('team_section_title', array(
-    'default' => '',
-    'sanitize_callback' => 'sanitize_text_field',
-  ));
-
-  $wp_customize->add_control('team_section_title', array(
-    'label' => 'Заголовок секції',
-    'section' => 'team_section',
-    'type' => 'text',
-  ));
-
-  for ($i = 1; $i <= 4; $i++) {
-    $wp_customize->add_setting('team_member_name_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'sanitize_text_field',
-    ));
-
-    $wp_customize->add_control('team_member_name_' . $i, array(
-      'label' => 'Ім\'я члена команди ' . $i,
-      'section' => 'team_section',
-      'type' => 'text',
-    ));
-
-    $wp_customize->add_setting('team_member_position_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'sanitize_text_field',
-    ));
-
-    $wp_customize->add_control('team_member_position_' . $i, array(
-      'label' => 'Посада члена команди ' . $i,
-      'section' => 'team_section',
-      'type' => 'text',
-    ));
-
-    $wp_customize->add_setting('team_member_photo_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'esc_url_raw',
-    ));
-
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_photo_' . $i, array(
-      'label' => 'Фото члена команди ' . $i . ' (desktop)',
-      'section' => 'team_section',
-    )));
-
-    $wp_customize->add_setting('team_member_photo_2x_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'esc_url_raw',
-    ));
-
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_photo_2x_' . $i, array(
-      'label' => 'Фото 2x члена команди ' . $i . ' (desktop)',
-      'section' => 'team_section',
-    )));
-
-    $wp_customize->add_setting('team_member_photo_768_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'esc_url_raw',
-    ));
-
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_photo_768_' . $i, array(
-      'label' => 'Фото члена команди ' . $i . ' (tablet)',
-      'section' => 'team_section',
-    )));
-
-    $wp_customize->add_setting('team_member_photo_768_2x_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'esc_url_raw',
-    ));
-
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_photo_768_2x_' . $i, array(
-      'label' => 'Фото 2x члена команди ' . $i . ' (tablet)',
-      'section' => 'team_section',
-    )));
-
-    $wp_customize->add_setting('team_member_photo_480_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'esc_url_raw',
-    ));
-
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_photo_480_' . $i, array(
-      'label' => 'Фото члена команди ' . $i . ' (mobile)',
-      'section' => 'team_section',
-    )));
-
-    $wp_customize->add_setting('team_member_photo_480_2x_' . $i, array(
-      'default' => '',
-      'sanitize_callback' => 'esc_url_raw',
-    ));
-
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_photo_480_2x_' . $i, array(
-      'label' => 'Фото 2x члена команди ' . $i . ' (mobile)',
-      'section' => 'team_section',
-    )));
-
-    $social_networks = array('instagram', 'twitter', 'facebook', 'linkedin');
-
-    foreach ($social_networks as $network) {
-      $wp_customize->add_setting('team_member_' . $network . '_' . $i, array(
-        'default' => '',
-        'sanitize_callback' => 'esc_url_raw',
-      ));
-
-      $wp_customize->add_control('team_member_' . $network . '_' . $i, array(
-        'label' => ucfirst($network) . ' для члена команди ' . $i,
-        'section' => 'team_section',
-        'type' => 'url',
-      ));
-    }
-  }
-
+// Callback функція для метабоксу тексту
+function service_card_text_callback($post) {
+  wp_nonce_field(basename(__FILE__), 'service_text_nonce');
+  $service_text = get_post_meta($post->ID, '_service_text', true);
+  ?>
+  <p>
+      <input type="text" name="service_text" value="<?php echo esc_attr($service_text); ?>" style="width:100%;" placeholder="Введіть текст, який буде відображатися на картці" />
+  </p>
+  <?php
 }
 
+// Зберігаємо метадані
+function save_service_meta($post_id) {
+  // Перевіряємо nonce для текстового поля
+  if (!isset($_POST['service_text_nonce']) || !wp_verify_nonce($_POST['service_text_nonce'], basename(__FILE__))) {
+      return $post_id;
+  }
+  
+  // Перевіряємо чи не автозбереження
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+      return $post_id;
+  }
+  
+  // Перевіряємо права користувача
+  if ('services' == $_POST['post_type'] && !current_user_can('edit_post', $post_id)) {
+      return $post_id;
+  }
+  
+  // Зберігаємо текст картки
+  if (isset($_POST['service_text'])) {
+      update_post_meta($post_id, '_service_text', sanitize_text_field($_POST['service_text']));
+  }
+}
+
+
+// Реєстрація типу запису "Команда"
+function register_team_post_type() {
+  $labels = array(
+      'name'               => 'Команда',
+      'singular_name'      => 'Член команди',
+      'menu_name'          => 'Команда',
+      'add_new'            => 'Додати нового',
+      'add_new_item'       => 'Додати нового члена команди',
+      'edit_item'          => 'Редагувати члена команди',
+      'new_item'           => 'Новий член команди',
+      'view_item'          => 'Переглянути члена команди',
+      'search_items'       => 'Шукати членів команди',
+      'not_found'          => 'Членів команди не знайдено',
+      'not_found_in_trash' => 'В кошику членів команди не знайдено',
+  );
+
+  $args = array(
+      'labels'             => $labels,
+      'public'             => true,
+      'publicly_queryable' => true,
+      'show_ui'            => true,
+      'show_in_menu'       => true,
+      'query_var'          => true,
+      'rewrite'            => array('slug' => 'team'),
+      'capability_type'    => 'post',
+      'has_archive'        => true,
+      'hierarchical'       => false,
+      'menu_position'      => 6,
+      'menu_icon'          => 'dashicons-groups',
+      'supports'           => array('title', 'thumbnail'),
+  );
+
+  register_post_type('team', $args);
+}
+add_action('init', 'register_team_post_type');
+
+// Додаємо метабокси для члена команди
+function team_meta_boxes() {
+  add_meta_box(
+      'team_member_position',
+      'Посада',
+      'team_member_position_callback',
+      'team',
+      'normal',
+      'high'
+  );
+  
+  add_meta_box(
+      'team_member_social',
+      'Соціальні мережі',
+      'team_member_social_callback',
+      'team',
+      'normal',
+      'high'
+  );
+}
+add_action('add_meta_boxes', 'team_meta_boxes');
+
+// Callback функція для метабоксу посади
+function team_member_position_callback($post) {
+  wp_nonce_field(basename(__FILE__), 'team_position_nonce');
+  $position = get_post_meta($post->ID, '_team_position', true);
+  ?>
+  <p>
+      <input type="text" name="team_position" value="<?php echo esc_attr($position); ?>" style="width:100%;" placeholder="Наприклад: Product Designer" />
+  </p>
+  <?php
+}
+
+// Callback функція для метабоксу соціальних мереж
+function team_member_social_callback($post) {
+  wp_nonce_field(basename(__FILE__), 'team_social_nonce');
+  $instagram = get_post_meta($post->ID, '_team_instagram', true);
+  $twitter = get_post_meta($post->ID, '_team_twitter', true);
+  $facebook = get_post_meta($post->ID, '_team_facebook', true);
+  $linkedin = get_post_meta($post->ID, '_team_linkedin', true);
+  ?>
+  <p>
+      <label for="team_instagram">Instagram:</label>
+      <input type="url" name="team_instagram" id="team_instagram" value="<?php echo esc_url($instagram); ?>" style="width:100%;" placeholder="https://instagram.com/username" />
+  </p>
+  <p>
+      <label for="team_twitter">Twitter:</label>
+      <input type="url" name="team_twitter" id="team_twitter" value="<?php echo esc_url($twitter); ?>" style="width:100%;" placeholder="https://twitter.com/username" />
+  </p>
+  <p>
+      <label for="team_facebook">Facebook:</label>
+      <input type="url" name="team_facebook" id="team_facebook" value="<?php echo esc_url($facebook); ?>" style="width:100%;" placeholder="https://facebook.com/username" />
+  </p>
+  <p>
+      <label for="team_linkedin">LinkedIn:</label>
+      <input type="url" name="team_linkedin" id="team_linkedin" value="<?php echo esc_url($linkedin); ?>" style="width:100%;" placeholder="https://linkedin.com/in/username" />
+  </p>
+  <?php
+}
+
+// Зберігаємо метадані члена команди
+function save_team_meta($post_id) {
+  // Перевіряємо nonce для поля посади
+  if (!isset($_POST['team_position_nonce']) || !wp_verify_nonce($_POST['team_position_nonce'], basename(__FILE__))) {
+      return $post_id;
+  }
+  
+  // Перевіряємо nonce для поля соціальних мереж
+  if (!isset($_POST['team_social_nonce']) || !wp_verify_nonce($_POST['team_social_nonce'], basename(__FILE__))) {
+      return $post_id;
+  }
+  
+  // Перевіряємо чи не автозбереження
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+      return $post_id;
+  }
+  
+  // Перевіряємо права користувача
+  if ('team' == $_POST['post_type'] && !current_user_can('edit_post', $post_id)) {
+      return $post_id;
+  }
+  
+  // Зберігаємо посаду
+  if (isset($_POST['team_position'])) {
+      update_post_meta($post_id, '_team_position', sanitize_text_field($_POST['team_position']));
+  }
+  
+  // Зберігаємо соціальні мережі
+  if (isset($_POST['team_instagram'])) {
+      update_post_meta($post_id, '_team_instagram', esc_url_raw($_POST['team_instagram']));
+  }
+  
+  if (isset($_POST['team_twitter'])) {
+      update_post_meta($post_id, '_team_twitter', esc_url_raw($_POST['team_twitter']));
+  }
+  
+  if (isset($_POST['team_facebook'])) {
+      update_post_meta($post_id, '_team_facebook', esc_url_raw($_POST['team_facebook']));
+  }
+  
+  if (isset($_POST['team_linkedin'])) {
+      update_post_meta($post_id, '_team_linkedin', esc_url_raw($_POST['team_linkedin']));
+  }
+}
+add_action('save_post_team', 'save_team_meta');
+add_action('save_post_services', 'save_service_meta');
+
+
 add_action('customize_register', 'theme_customize_register');
+
+
 
 
 add_action('after_setup_theme', 'mytheme_setup');
